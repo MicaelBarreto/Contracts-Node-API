a64ca421422e44520115c2a0c298f018d126e41b
Micaelbarreto <micaelplayer98@gmail.com>
2019-11-06 14:30:31 -0200

    tests

--- test/Users.test.js
+++ test/Users.test.js
@@ -2,52 +2,65 @@ const should = require('should');
 const axios = require('axios');
 const chai = require('chai');
 const expect = chai.expect;
-const urlBase = 'http://localhost:3000';
+const urlBase = 'http://localhost:3000/users';
 const getToken = require('./Token.test').getToken;
-var id;
-
 
 describe('Users test API calls',function(){
+
     it('Retrieve all Users', function(){
         return getToken()
                 .then(token => {
-                    axios.get(urlBase+'/users', { headers: { auth: token } })
-                        .then(res => {
-                            response = res.data;
-                            expect(res.status).to.equal(200);
-                            expect(typeof response).to.equal('object');
-                            expect(response).to.have.lengthOf.at.least(2);
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
+                    axios.get(urlBase, { headers: { auth: token } })
+                    .then(res => {
+                        response = res.data;
+                        expect(res.status).to.equal(200);
+                        expect(typeof response).to.equal('object');
+                        expect(response).to.have.lengthOf.at.least(2);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
                 });
     });
 
     it('Create new User', function(){
         return getToken()
                 .then(token => {
-                    axios.post(urlBase+'/users', {
+                    axios.post(urlBase, {
                         email: 'test@newtest.com',
                         password: '123465'
                     },
                     { headers: { auth: token } })
-                        .then(res => {
-                            response = res.data;
-                            id = res.data.id;
-                            expect(res.status).to.equal(201);
-                            expect(typeof response).to.equal('object');
-                            expect(response).to.have.lengthOf.at.least(2);
-                            return id;
-                        })
-                        .catch(err => {
-                            console.log(err);
-                        });
+                    .then(res => {
+                        response = res.data;
+                        console.log(response)
+                        expect(res.status).to.equal(201);
+                        expect(typeof response).to.equal('object');
+                        expect(response).to.have.lengthOf.at.least(2);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
                 });
     });
 
-    it('Update User', function(done){
-        done();
+    it('Update User', function(){
+        return getToken()
+                .then(token => {
+                    axios.put(urlBase+'/'+id, {
+                        email: 'test@newtest.com',
+                        password: '1234657'
+                    },
+                    { headers: { auth: token } })
+                    .then(res => {
+                        response = res.data;
+                        expect(res.status).to.equal(200);
+                        expect(typeof response).to.equal('object');
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+                });
     });
 
     it('Delete User', function(done){
